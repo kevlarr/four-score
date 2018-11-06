ecd892eed9dbddf9277a255b7723da8532f4df14
kevlarr <echo1+git@dvnl.net>
2018-11-05 22:43:19 -0500

    Get and verify board choice

--- rust_/src/main.rs
+++ rust_/src/main.rs
@@ -1,18 +1,24 @@
 use std::io;
 use std::io::Write;
 
+type Input<T> = Result<T, io::Error>;
+
+#[derive(Debug)]
 struct Players(pub String, pub String);
+
+#[derive(Debug)]
+struct Board(pub u8);
 
 fn main() {
     println!("Welcome to FourScore!\n");
 
-    match new_players() {
-        Ok(players) => println!("{} and {}, sitting in a tree...", players.0, players.1),
-        Err(e) => println!("Error: {:?}", e),
-    }
+    let players = new_players().unwrap();
+    let board = new_board().unwrap();
+
+    println!("Players {:?}, playing with board {:?}", players, board);
 }
 
-fn new_players() -> Result<Players, io::Error> {
+fn new_players() -> Input<Players> {
     let first = input("First player")?;
     let mut second = input("Second player")?;
 
@@ -23,7 +29,29 @@ fn new_players() -> Result<Players, io::Error> {
     Ok(Players(first, second))
 }
 
-fn input(prompt: &str) -> Result<String, io::Error> {
+fn new_board() -> Input<Board> {
+    let choice = input_range(
+        "Play with a (1) standard or (2) custom board",
+        1, 2)?;
+
+    Ok(Board(choice))
+}
+
+fn input_range(prompt: &str, x: u8, y: u8) -> Input<u8> {
+    let mut i = input(prompt)?;
+
+    loop {
+        if let Ok(n) = i.parse::<u8>() {
+            if n >= x && n <= y {
+                return Ok(n);
+            }
+        }
+
+        i = input("Please enter a valid choice")?;
+    }
+}
+
+fn input(prompt: &str) -> Input<String> {
     print!("{}: ", prompt);
 
     // Need to print and then flush to keep read_line on same line
