96d67125d5c6331699c1bb332e4004a794630b07
kevlarr <echo1+git@dvnl.net>
2018-11-17 21:04:42 -0500

    All checks use same function

--- rust_/src/types/board.rs
+++ rust_/src/types/board.rs
@@ -46,85 +46,50 @@ impl Board {
         MoveResult::ColumnFull
     }
 
-    /// Bottom row is highest index
-    ///        0  1  .. w-1
-    ///    0 |‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-    ///    1 |
-    ///    2 |
-    ///   .. |
-    ///  h-1 |
-    ///
     fn won(&self, row: usize, col: usize) -> bool {
         self.won_vertically(row, col) || self.won_horizontally(row, col) ||
         self.won_diagonally(row, col) || self.won_antidiagonally(row, col)
     }
 
-    /// Check for win down rows
     fn won_vertically(&self, row: usize, col: usize) -> bool {
-        // Only bother checking if it's at least the 4th token played in col
-        if row > self.height - 4 { return false; }
-
-        (row + 1 .. row + 4)
-            .map(|i| self.rows[i][col])
-            .find(|cell| cell != &self.rows[row][col])
-            .is_none()
+        self.count_matches(row, 1, col, 0, 0) > 2 // "down"
     }
 
-    /// Check for win across columns
     fn won_horizontally(&self, row: usize, col: usize) -> bool {
-        let min = if col < 4 { 0 } else { col - 3 };
-        let max = if col > self.width - 4 { self.width } else { col + 3 };
-        let mut in_a_row = 0;
-
-        for i in min..max {
-            in_a_row = if self.rows[row][i] == self.rows[row][col] {
-                in_a_row + 1
-            } else {
-                0
-            };
-
-            if in_a_row > 3 {
-                return true;
-            }
-        }
-
-        false
+        self.count_matches(row, 0, col, -1, 0) +  // left
+        self.count_matches(row, 0, col, 1, 0) > 2 // right
     }
 
     /// Check for win in \ direction
     fn won_diagonally(&self, row: usize, col: usize) -> bool {
-        let token = self.rows[row][col].expect("token should be present");
-
-        self.count_matches(&token, row, -1, col, -1, 0) + // "up" and left
-        self.count_matches(&token, row, 1, col, 1, 0) > 2 // "down" and right
+        self.count_matches(row, -1, col, -1, 0) + // "up" and left
+        self.count_matches(row, 1, col, 1, 0) > 2 // "down" and right
     }
 
     /// Check for win in / direction
     fn won_antidiagonally(&self, row: usize, col: usize) -> bool {
-        let token = self.rows[row][col].expect("token should be present");
-
-        self.count_matches(&token, row, 1, col, -1, 0) + // "down" and left
-        self.count_matches(&token, row, -1, col, 1, 0) > 2 // "up" and right
+        self.count_matches(row, 1, col, -1, 0) +    // "down" and left
+        self.count_matches(row, -1, col, 1, 0) > 2 // "up" and right
     }
 
     fn count_matches(&self,
-        token: &Token,
         row: usize, dr: isize,
         col: usize, dc: isize,
         acc: usize,
     ) -> usize {
-        let new_row = (row as isize + dr) as usize;
-        let new_col = (col as isize + dc) as usize;
+        let next_row = (row as isize + dr) as usize;
+        let next_col = (col as isize + dc) as usize;
 
         // -1 will wrap to a big number, so only need to check if greater than
-        if new_row >= self.height || new_col >= self.width { return acc; }
-
-        match &self.rows[new_row][new_col] {
-            Some(t) if t == token =>
-                self.count_matches(token, new_row, dr, new_col, dc, acc + 1),
-            _ =>
-                acc,
+        if next_row >= self.height || next_col >= self.width {
+            return acc;
         }
+
+        if self.rows[next_row][next_col] == self.rows[row][col] {
+            return self.count_matches(next_row, dr, next_col, dc, acc + 1);
+        }
+
+        acc
     }
 
     fn draw(&self, row: usize) -> bool {
